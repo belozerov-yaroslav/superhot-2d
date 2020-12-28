2538091ab5e23001b247849f66fbc864a201952c
Echko <thd>
2020-12-28 21:41:13 +0500

    новый текстур пак

--- config.py
+++ config.py
@@ -9,14 +9,17 @@ shot_button = pygame.K_e
 
 sprite_folder_name = 'pic2'
 player_sprite = 'pers2.png'
-wall_sprite = 'cell1_with_box_tile.jpg'
+wall_sprite = 'cell1_with_box_tile.png'
 enemy_sprite = 'enemy.png'
-field_sprite = 'cell1_tile.jpg'
-lazer_sprite = 'cell1_tile_with_lazer.jpg'
-pepl_sprite = 'pepl.jpg'
-boom_sprite = 'cell1_tile_with_boom.jpg'
-pepl_boom_sprite = 'pepel_boom.jpg'
+field_sprite = 'cell1_tile.png'
+lazer_sprite = 'cell1_tile_with_lazer.png'
+pepl_sprite = 'pepl.png'
+boom_sprite = 'cell1_tile_with_boom.png'
+pepl_boom_sprite = 'pepel_boom.png'
 game_over_sprite = 'game_over.png'
+game_win_screen = 'game_win.png'
+enemy_lazer_sprite = 'tile_with_red_laser_enemy.png'
+enemy_pepl_sprite = 'pepl_enemy.png'
 debug_mode = 1
 
 cell_size = 48  # в пикселях
